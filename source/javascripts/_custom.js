6a4d54bc6c0f21b2d6988fa57f9707b5845acaf3
dannyvassallo <danielvassallo87@gmail.com>
2016-03-11 15:38:37 -0500

    added a bunch of PTV styles

--- source/javascripts/_custom.js
+++ source/javascripts/_custom.js
@@ -1,2 +1,16 @@
-// ADD YOUR JS HERE
-console.log('loaded');
+function resizeGradient(){
+  var videoH = $('.video').height();
+  var gradient = $('.gradient');
+  gradient.height(videoH);
+  var newMargin = videoH - 5;
+  var cssMargin = "-"+newMargin+"px";
+  gradient.css('margin-top',cssMargin);
+}
+
+$(function(){
+  resizeGradient();
+});
+
+$(window).resize(function(){
+  resizeGradient();
+});
