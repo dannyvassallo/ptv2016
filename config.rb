8ef5a94a2f2f3adb2d9b9ccae8f7c805f1aeeb1a
dannyvassallo <danielvassallo87@gmail.com>
2016-03-17 13:38:43 -0400

    changed to match psd

--- config.rb
+++ config.rb
@@ -49,7 +49,7 @@ configure :build do
   # Minify CSS on build
   activate :minify_css
   # Minify HTML on build
-  activate :minify_html
+  # activate :minify_html
   # Minify Javascript on build
   activate :minify_javascript
 end
---
+++ source/javascripts/_custom-original.js
@@ -0,0 +1,95 @@
+// function resizeGradient(){
+//   var videoH = $('.video').height();
+//   var gradient = $('.gradient');
+//   gradient.height(videoH);
+//   var newMargin = videoH - 5;
+//   var cssMargin = "-"+newMargin+"px";
+//   gradient.css('margin-top',cssMargin);
+// }
+
+// function resizeReleaseDate(){
+//   var naH = $('.newalbum').height();
+//   var rd = $('.releasedate');
+//   rd.height(naH);
+// }
+
+// $(function(){
+//   resizeGradient();
+//   resizeReleaseDate();
+//   $("#ytEmbed").on("load", function () {
+//       resizeReleaseDate();
+//   });
+// });
+
+// $(window).resize(function(){
+//   setTimeout(function(){
+//     resizeGradient();
+//     resizeReleaseDate();
+//   }, 500);
+// });
+
+// // iterate through errors and growl them
+// function toasts(){
+//   setTimeout(function(){
+//     $('span.error').each(function(){
+//       if($(this).html() != ""){
+//         var errorText = $(this).text();
+//         Materialize.toast(errorText, 4000, 'red');
+//       }
+//     });
+//   }, 100);
+// }
+
+// // successMsg Constructor
+// var successMsg = "<div id=\"thankyou\" class=\"col-xs-12 black-text\"><h2 class=\"thanks\">Thank you for entering!</h2></div>";
+
+
+// $("#email-form").validate({
+//   ignore: "",
+//   focusInvalid: false,
+//   rules: {
+//     'entry.2137816727': {
+//       required: {
+//         depends:function(){
+//           $(this).val($.trim($(this).val()));
+//           return true;
+//         }
+//       },
+//       email: true
+//     }
+//   },
+//   messages: {
+//     'entry.2137816727': {
+//       required: "Please enter your email address.",
+//     }
+//   },
+//   invalidHandler: function(form, validator) {
+//     toasts();
+//   },
+//   success: "valid",
+//   submitHandler: function(form) {
+//     form.submit();
+//     setTimeout(function(){
+//       $('form input').val('');
+//       $('form input').focus();
+//       $('form input').blur();
+//     }, 500);
+//     Materialize.toast("Thanks for your entry!", 4000, 'green');
+//   },
+//   errorElement : 'span',
+//   errorPlacement: function (error, element) {
+//     var elementId = element[0].id;
+//     if(elementId == 'fbshareinput' || elementId == 'tour-input'){
+//       error.insertAfter($('#'+elementId).next('.error-box'));
+//     } else {
+//       error.insertAfter($(element).parent().next('.error-box'));
+//     }
+//   }
+// });
+
+
+// $(function(){
+//   $('form label').on('click', function(){
+//     $(this).prev('input').focus();
+//   });
+// });
